5eb70b4d4e806faa15519258a7a222b824ae4390
KamiyaNya <dikriss16@gmail.com>
2022-10-07 20:10:39 +0300

    change 1

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const fastify = require("fastify")({ logger: true });
 
 // Declare a route
 fastify.get("/test", async (request, reply) => {
-  return { hello: "world1" };
+	reply.send({ hello: "world" });
 });
 
 // Run the server!
 const start = async () => {
-  try {
-    await fastify.listen({ port: 8080 });
-  } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
+	try {
+		await fastify.listen({ port: 8080 });
+	} catch (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
 };
 
 start();
