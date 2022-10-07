9c0d8704b4ef7d02213a04c84c472a4659c4e1ff
ToushiroHotoru <toushirohotoru@gmail.com>
2022-10-07 19:31:07 +0300

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const fastify = require("fastify")({ logger: true });
 
 // Declare a route
 fastify.get("/test", async (request, reply) => {
-  return { hello: "world" };
+  return { hello: "world1" };
 });
 
 // Run the server!
