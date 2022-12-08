5cc5615335482898dd8b84b32d5e02d6702d637f
Kamiya_Nya <dikriss16@gmail.com>
2022-12-08 12:53:09 +0300

    Контроллер для тегов

---
+++ controller/Tags.controller.js
@@ -0,0 +1,11 @@
+const chalk = require("chalk");
+const Tags = require("../models/Tags.modal");
+
+
+class TagsController{
+  async addTags(request, reply){
+
+  }
+}
+
+module.exports = new TagsController();
