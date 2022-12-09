c1052eda95249d459881318ce2f3ee289b0bb829
Kamiya_Nya <dikriss16@gmail.com>
2022-12-09 17:04:03 +0300

    Добавил маршрут для получения всех тэгов

--- api/TagRoutes.js
+++ api/TagRoutes.js
@@ -1,6 +1,8 @@
 const TagsController = require("../controller/Tags.controller");
 
 async function TagRoutes(fastify, options) {
+  fastify.get("/get_tags", TagsController.getAllTags);
+
   fastify.post("/write_tags", TagsController.addTags);
 
   fastify.delete("/delete_tags", TagsController.deleteTags);
