9a5fdac01097b1def20e2d5c680679cb5d8b330a
Kamiya_Nya <dikriss16@gmail.com>
2022-12-09 13:57:32 +0300

    Поменял передачу данных

--- controller/Tags.controller.js
+++ controller/Tags.controller.js
@@ -3,10 +3,10 @@ const Tags = require("../models/Tags.model");
 
 class TagsController {
   async addTags(request, reply) {
-    const tag1 = await Tags.add("Школьницы");
-    const tag2 = await Tags.add("В общественном месте");
-    const tag3 = await Tags.add("Мерзкий дядька");
-    const tag4 = await Tags.add("Netorare");
+    const tag1 = await Tags.add({ name: "Школьницы" });
+    const tag2 = await Tags.add({ name: "В общественном месте" });
+    const tag3 = await Tags.add({ name: "Мерзкий дядька" });
+    const tag4 = await Tags.add({ name: "Netorare" });
     reply.code(200).send({ tag1, tag2, tag3, tag4 });
   }
 
