7e2c0f4e7fb023d7622f475f431e2cb6d5b5253e
Kamiya_Nya <dikriss16@gmail.com>
2022-12-09 13:39:41 +0300

    Рандомно выбирается дефолтная картинка для большого тега и мини тега

--- models/Tags.model.js
+++ models/Tags.model.js
@@ -1,29 +1,44 @@
 const { model, Schema } = require("mongoose");
-const os = require("os");
 
 const defaultTagsImages = [
   "/public/tags/tags_default_1.jpg",
   "/public/tags/tags_default_2.jpg",
   "/public/tags/tags_default_3.jpg",
 ];
+const defaultTagsImagesMini = [
+  "/public/tags/mini_tags_default_1.jpg",
+  "/public/tags/mini_tags_default_2.jpg",
+  "/public/tags/mini_tags_default_3.jpg",
+];
 
 const tagsSchema = new Schema({
   image: {
     type: String,
     default: function () {
-      return (
-        os.hostname() +
-        defaultTagsImages[Math.floor(Math.random() * defaultTagsImages.length)]
-      );
+      return defaultTagsImages[
+        Math.floor(Math.random() * defaultTagsImages.length)
+      ];
     },
   },
-  name: { type: String, required: true },
+  miniImage: {
+    type: String,
+    default: function () {
+      return defaultTagsImagesMini[
+        Math.floor(Math.random() * defaultTagsImagesMini.length)
+      ];
+    },
+  },
+  name: { type: String, required: true, unique: true },
   description: { type: String },
   count: { type: Number, default: 0 },
 });
 
 tagsSchema.statics.add = function (name, image = "", description = "") {
-  this.create(name, image, description);
+  return this.create({ name: name.toLowerCase() });
+};
+
+tagsSchema.statics.deleteAll = function (name, image = "", description = "") {
+  return this.deleteMany({});
 };
 
 module.exports = model("Tags", tagsSchema);
