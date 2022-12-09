3f8f3c0e96727d4f8498999bbfbb979a4cc7dde9
Kamiya_Nya <dikriss16@gmail.com>
2022-12-09 13:39:06 +0300

    Добавил контроллер для добавления тега и удаления всех тегов

--- controller/Tags.controller.js
+++ controller/Tags.controller.js
@@ -1,10 +1,18 @@
 const chalk = require("chalk");
 const Tags = require("../models/Tags.model");
 
+class TagsController {
+  async addTags(request, reply) {
+    const tag1 = await Tags.add("Школьницы");
+    const tag2 = await Tags.add("В общественном месте");
+    const tag3 = await Tags.add("Мерзкий дядька");
+    const tag4 = await Tags.add("Netorare");
+    reply.code(200).send({ tag1, tag2, tag3, tag4 });
+  }
 
-class TagsController{
-  async addTags(request, reply){
-
+  async deleteTags(request, reply) {
+    const tag = await Tags.deleteAll();
+    reply.code(200).send({ tag });
   }
 }
 
