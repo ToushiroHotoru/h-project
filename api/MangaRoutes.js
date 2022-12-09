9957b50082e3ef1e6557edaed1669ccdd62464a8
Kamiya_Nya <dikriss16@gmail.com>
2022-12-09 13:37:21 +0300

    Маршрут для манг

---
+++ api/MangaRoutes.js
@@ -0,0 +1,21 @@
+const MangaController = require("../controller/Manga.controller");
+
+async function MangaRoutes(fastify, options) {
+  fastify.get("/mangas", MangaController.getAllMangas);
+
+  fastify.get("/get_paths", MangaController.getMangasId);
+
+  fastify.post("/write", MangaController.mangaAppendOne);
+
+  fastify.post("/write-many", MangaController.mangaAppendMany);
+
+  fastify.post("/manga-static", MangaController.getStatic);
+
+  fastify.post("/manga-dynamic", MangaController.getDynamic);
+
+  fastify.delete("/delete-one", MangaController.deleteMangaById);
+
+  fastify.delete("/delete-many", MangaController.deleteMangaAll);
+}
+
+module.exports = MangaRoutes;
