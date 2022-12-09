c290fb3359ce294b2dd2eeb81ac4c0c94a37680d
Kamiya_Nya <dikriss16@gmail.com>
2022-12-09 13:36:40 +0300

    Разделил маршруты по отдельным файлам

--- api/routes.js
+++ api/routes.js
@@ -1,25 +1,11 @@
-const chalk = require('chalk');
-const Index = require('../controller/Index.controller');
-const MangaController = require('../controller/Manga.controller');
+const IndexRoutes = require("./IndexRoutes");
+const MangaRoutes = require("./MangaRoutes");
+const TagsRoutes = require("./TagRoutes");
 
 async function routes(fastify, options) {
-	fastify.get('/', Index.welcomePage);
-
-	fastify.get('/mangas', MangaController.getAllMangas);
-
-	fastify.get('/get_paths', MangaController.getMangasId);
-
-	fastify.post('/write', MangaController.mangaAppendOne);
-
-	fastify.post('/write-many', MangaController.mangaAppendMany);
-
-	fastify.post('/manga-static', MangaController.getStatic);
-
-	fastify.post('/manga-dynamic', MangaController.getDynamic);
-
-	fastify.delete('/delete-one', MangaController.deleteMangaById);
-
-	fastify.delete('/delete-many', MangaController.deleteMangaAll);
+  fastify.register(IndexRoutes);
+  fastify.register(MangaRoutes);
+  fastify.register(TagsRoutes);
 }
 
 module.exports = routes;
