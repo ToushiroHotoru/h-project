1ae8f3b2f89cedf443a306464d495cf4d6df6d04
ToushiroHotoru <60753059+ToushiroHotoru@users.noreply.github.com>
2023-01-02 12:59:27 +0000

    Добавил теги, чуть поменял ендпойнты тегов

--- api/tags/Tags.controller.js
+++ api/tags/Tags.controller.js
@@ -2,25 +2,24 @@ const Tags = require("../../schemas/Tags.schema");
 
 class TagsController {
   async addTags(request, reply) {
-    const tag1 = await Tags.add({ name: "Школьницы" });
-    const tag2 = await Tags.add({ name: "В общественном месте" });
-    const tag3 = await Tags.add({ name: "Мерзкий дядька" });
-    const tag4 = await Tags.add({ name: "Netorare" });
-    reply.code(200).send({ tag1, tag2, tag3, tag4 });
+    const { image, miniImage, name, description } = request.body;
+    await Tags.add({image, miniImage, name, description});
+    reply.code(200).send({ msg: "tag has been created" });
   }
 
   async deleteTags(request, reply) {
-    const tags = await Tags.deleteAll();
-    reply.code(200).send({ tags });
+    await Tags.deleteAll();
+    reply.code(200).send({ msg: "all tags has been removed" });
   }
 
   async getAllTags(request, reply) {
     let tags = await Tags.getAll();
-    tags = tags.map((tag) => {
-      tag.image = "http://localhost:8080" + tag.image;
-      tag.miniImage = "http://localhost:8080" + tag.miniImage;
-      return tag;
-    });
+    // для локального сервера
+    // tags = tags.map((tag) => {
+    //   tag.image = "http://localhost:8080" + tag.image;
+    //   tag.miniImage = "http://localhost:8080" + tag.miniImage;
+    //   return tag;
+    // });
     reply.code(200).send({ tags });
   }
 }
