d0bd75d4e01c9949325db9fe12d2fe87754b0379
Kamiya_Nya <dikriss16@gmail.com>
2023-03-20 17:38:50 +0300

    Поменял пароль

--- plugins/mongoose.js
+++ plugins/mongoose.js
@@ -4,12 +4,11 @@ const chalk = require("chalk");
 async function mongooseInit(fastify, opts, done) {
   try {
     await mongoose.connect(
-      "mongodb+srv://h-project:02hKNQrsXpmdoK84@h-project-manga.okzes.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://h-project:NaDSwTgyCZxOFa2F@h-project-manga.okzes.mongodb.net/?retryWrites=true&w=majority"
     );
   } catch (err) {
     console.log(`db connection error - ${chalk.red(err)}`);
   }
-
   done();
 }
 
