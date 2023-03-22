d1499e63254188d958d724779bc6ca7fc1d81c6c
Kamiya_Nya <dikriss16@gmail.com>
2023-03-22 15:02:04 +0300

    Добавлен маршрут для логина

--- routes/User.routes.js
+++ routes/User.routes.js
@@ -1,17 +1,17 @@
 const UserController = require("../api/user/User.controller");
 
 async function TagRoutes(fastify, options) {
-
   fastify.get("/all-users", UserController.getAllUsers);
 
   fastify.post("/registration", UserController.registerUser);
+
+  fastify.post("/login", UserController.loginUser);
 
   fastify.post("/set_preferences_tags", UserController.setPreferencesTags);
 
   fastify.post("/set_exceptions_tags", UserController.setExceptionsTags);
 
   fastify.post("/set_avatar", UserController.setAvatar);
-
 }
 
 module.exports = TagRoutes;
