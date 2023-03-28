83082b1c5a06046b9e50bacb61a4382b2f0b323c
Kamiya_Nya <dikriss16@gmail.com>
2023-03-28 12:23:29 +0300

    Мелкие изменения

--- schemas/Manga.schema.js
+++ schemas/Manga.schema.js
@@ -1,4 +1,4 @@
-const { model, Schema } = require('mongoose');
+const { model, Schema } = require("mongoose");
 
 const MangaSchema = new Schema(
   {
@@ -6,10 +6,13 @@ const MangaSchema = new Schema(
     cover: { type: String, required: true },
     artist: { type: String, required: true },
     series: { type: String, required: true },
-    tags: [{
-      type: Schema.Types.ObjectId,
-      ref: "Tags", required: true
-    }],
+    tags: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Tags",
+        required: true,
+      },
+    ],
     likes: { type: Number, required: true, default: 0 },
     views: { type: Number, required: true, default: 0 },
     cycle: {
@@ -21,67 +24,84 @@ const MangaSchema = new Schema(
   { timestamps: true }
 );
 
-MangaSchema.statics.add = function(data) {
-  this.create(data);
+MangaSchema.statics.add = function (data) {
+  return this.create(data);
 };
 
-MangaSchema.statics.getAllMangasId = function() {
-  return this.find({}).select('_id');
+MangaSchema.statics.getAllMangasId = function () {
+  return this.find({}).select("_id");
 };
 
-MangaSchema.statics.sortByTime = function(offset, step, tags) {
-  if (tags) {
-    if (Array.isArray(tags)) {
-      return this.find({ tags: { $all: [...tags] } }).sort({ createdAt: 'desc' }).skip(offset).limit(step).populate("tags").exec();
-    }
-    return this.find({ tags: tags }).sort({ createdAt: 'desc' }).skip(offset).limit(step).populate("tags").exec();
-  }
-  return this.find({}).sort({ createdAt: 'desc' }).skip(offset).limit(step).populate("tags").exec();
-};
-
-MangaSchema.statics.sortByAlphabet = function(offset, step, tags) {
+MangaSchema.statics.sortByTime = function (offset, step, tags) {
   if (tags) {
     if (Array.isArray(tags)) {
       return this.find({ tags: { $all: [...tags] } })
-        .collation({ locale: 'en', strength: 2 })
-        .sort({ title: 1 })
+        .sort({ createdAt: "desc" })
         .skip(offset)
-        .limit(step).populate("tags").exec();
+        .limit(step)
+        .populate("tags")
+        .exec();
     }
     return this.find({ tags: tags })
-      .collation({ locale: 'en', strength: 2 })
-      .sort({ title: 1 })
+      .sort({ createdAt: "desc" })
       .skip(offset)
-      .limit(step).populate("tags").exec();
-
+      .limit(step)
+      .populate("tags")
+      .exec();
   }
   return this.find({})
-    .collation({ locale: 'en', strength: 2 })
-    .sort({ title: 1 })
+    .sort({ createdAt: "desc" })
     .skip(offset)
-    .limit(step).populate("tags").exec();
+    .limit(step)
+    .populate("tags")
+    .exec();
 };
 
-MangaSchema.statics.getStaticFields = function(id) {
+MangaSchema.statics.sortByAlphabet = function (offset, step, tags) {
+  if (tags) {
+    if (Array.isArray(tags)) {
+      return this.find({ tags: { $all: [...tags] } })
+        .collation({ locale: "en", strength: 2 })
+        .sort({ title: 1 })
+        .skip(offset)
+        .limit(step)
+        .populate("tags")
+        .exec();
+    }
+    return this.find({ tags: tags })
+      .collation({ locale: "en", strength: 2 })
+      .sort({ title: 1 })
+      .skip(offset)
+      .limit(step)
+      .populate("tags")
+      .exec();
+  }
+  return this.find({})
+    .collation({ locale: "en", strength: 2 })
+    .sort({ title: 1 })
+    .skip(offset)
+    .limit(step)
+    .populate("tags")
+    .exec();
+};
+
+MangaSchema.statics.getStaticFields = function (id) {
   return this.findById(id).select([
-    'title',
-    'cover',
-    'artist',
-    'series',
-    'cycle',
+    "title",
+    "cover",
+    "artist",
+    "series",
+    "cycle",
   ]);
 };
 
-MangaSchema.statics.getDynamicFields = function(id) {
-  return this.findById(id).select([
-    '-title',
-    '-cover',
-    '-artist',
-    '-series',
-    '-cycle',
-  ]).populate({
-    path: "tags",
-  }).exec();
+MangaSchema.statics.getDynamicFields = function (id) {
+  return this.findById(id)
+    .select(["-title", "-cover", "-artist", "-series", "-cycle"])
+    .populate({
+      path: "tags",
+    })
+    .exec();
 };
 
-module.exports = model('Manga', MangaSchema);
+module.exports = model("Manga", MangaSchema);
