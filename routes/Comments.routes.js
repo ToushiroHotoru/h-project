eb4b7b31acb40be64b054f794b14f7cf012e6318
Kamiya_Nya <dikriss16@gmail.com>
2023-04-10 12:16:53 +0300

    Маршрут для получения комментов

--- routes/Comments.routes.js
+++ routes/Comments.routes.js
@@ -2,6 +2,7 @@ const commentsController = require("../api/comments/Comments.controller.js");
 
 async function CommentsRoutes(fastify, options) {
   fastify.post("/add_comment", commentsController.addComment);
+  fastify.get("/get_comments", commentsController.getMangaComments);
 }
 
 module.exports = CommentsRoutes;
