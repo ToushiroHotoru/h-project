fcbbf7567a21a83e4128e792acea9796bd55eb2c
Kamiya_Nya <dikriss16@gmail.com>
2023-04-10 12:03:09 +0300

    Добавил маршруты комментов в общий контроллер маршрутов

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ const TagsRoutes = require("./Tag.routes");
 const UserRoutes = require("./User.routes");
 const AvatarRoutes = require("./Avatar.routes");
 const UserPrivateRoutes = require("./UserPrivate.routes");
+const CommentsRoute = require("./Comments.routes");
 
 async function routes(fastify, options) {
   fastify.register(IndexRoutes);
@@ -12,6 +13,7 @@ async function routes(fastify, options) {
   fastify.register(AvatarRoutes);
   fastify.register(UserRoutes);
   fastify.register(UserPrivateRoutes);
+  fastify.register(CommentsRoute);
 }
 
 module.exports = routes;
